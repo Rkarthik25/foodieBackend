ac413d9334670dd7f7f750706b513d68f9f284ef
Vigneshprabu1398 <76090127+Vigneshprabu1398@users.noreply.github.com>
2022-05-18 00:58:24 +0530

    heroku for Restaurants

--- src/contollers/Location.controller.js
+++ src/contollers/Location.controller.js
@@ -14,7 +14,7 @@ router.get("/location", async(req, res)=>{
     return res.status(201).send(location);
 })
 
-router.delete("/location/:_id",async(req,res)=>{
+router.delete("/location/:id",async(req,res)=>{
     try{
         const location = await Location.findOneAndDelete({id:req.params.id})
         res.status(200).send(location)
